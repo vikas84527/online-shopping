fb7f958832c0600e0422feebf42304a2518a1a33
vikas84527 <vs972858@gmail.com>
2018-12-10 12:26:46 +0530

    setup

---
+++ shoppingbackend/src/main/java/com/vikas/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.vikas.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
